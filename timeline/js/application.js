d4a715c243e06bed477d270bcda5d85a4b4bf998
Phil LaPier <phil@thoughtbot.com>
2012-10-11 00:27:49 -0400

    Updated timeline

--- timeline/js/application.js
+++ timeline/js/application.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
       var images = $(this).find("section.post-content img");
       var dominantColor = getDominantColor(images);
 
-      $(this).find("section.post-meta").css('color', 'rgba('+ dominantColor +', 1)');
+      $(this).find("section.post-meta h2").css('color', 'rgba('+ dominantColor +', 1)');
   });
 });
