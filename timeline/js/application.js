099aa7c0d9f5d6d6e9df3211b3be47d707b4989b
Phil LaPier <phil@thoughtbot.com>
2012-10-12 17:09:56 -0400

    Changed to my own server to handle the json request

--- timeline/js/application.js
+++ timeline/js/application.js
@@ -1,7 +1,18 @@
 $(window).load(function () {
   $("article.photo").each(function() {
-      var images = $(this).find("section.post-content img");
-      var dominantColor = getDominantColor(images);
-      $(this).find("section.post-meta h2").css('color', 'rgba('+ dominantColor +', 1)');
+    var images = $(this).find("section.post-content img");
+    var image_url = images.attr("src");
+    var dominantColor = getDominantColor(images);
+    $.getImageData({
+      url: image_url,
+      success: function(image){
+        // Do something with the now local version of the image
+        alert(image);
+      },
+      error: function(xhr, text_status){
+        // Handle your error here
+      }
+    });
+    $(this).find("section.post-meta h2").css('color', 'rgba('+ dominantColor +', 1)');
   });
 });
--- timeline/js/getImageData.js
+++ timeline/js/getImageData.js
@@ -29,69 +29,69 @@ f=d("head")[0]||document.documentElement,q={},S=0,p,C={callback:L,url:location.h
 
 (function( $ ){
 
-	// jQuery getImageData Plugin
-	$.getImageData = function(args) {
-	
-		var regex_url_test = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-	
-		// If a URL has been specified
-		if(args.url) {
-		
-			// Ensure no problems when using http or http
-			var is_secure = location.protocol === "https:";
-			var server_url = "";
-		
-			// If url specified and is a url + if server is secure when image or user page is
-			if(args.server && regex_url_test.test(args.server) && (args.server.indexOf('https:') && (is_secure || args.url.indexOf('https:')))) {
-				server_url = args.server;
-			} else server_url = "//img-to-json.appspot.com/";
-		
-			server_url += "?callback=?";
-		
-			// Using jquery-jsonp (http://code.google.com/p/jquery-jsonp/) for the request
-			// so that errors can be handled
-			$.jsonp({	
-				url: server_url,
-				data: { url: escape(args.url) },
-				dataType: 'jsonp',
-				timeout: 10000,
-				// It worked!
-				success: function(data, status) {
-			
-					// Create new, empty image
-					var return_image = new Image();
-				
-					// When the image has loaded
-					$(return_image).load(function(){
-					
-						// Set image dimensions
-						this.width = data.width;
-						this.height = data.height;
-					
-						// Return the image
-						if(typeof(args.success) == typeof(Function)) {
-							args.success(this);
-						}
-					
-					// Put the base64 encoded image into the src to start the load
-					}).attr('src', data.data);
-				
-			  },
-				// Something went wrong.. 
-				error: function(xhr, text_status){
-					// Return the error(s)
-					if(typeof(args.error) == typeof(Function)) {
-						args.error(xhr, text_status);
-					}
-				}
-			});
-		
-		// No URL specified so error
-		} else {
-			if(typeof(args.error) == typeof(Function)) {
-				args.error(null, "no_url");
-			}
-		}
-	};
+  // jQuery getImageData Plugin
+  $.getImageData = function(args) {
+  
+    var regex_url_test = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  
+    // If a URL has been specified
+    if(args.url) {
+    
+      // Ensure no problems when using http or http
+      var is_secure = location.protocol === "https:";
+      var server_url = "";
+    
+      // If url specified and is a url + if server is secure when image or user page is
+      if(args.server && regex_url_test.test(args.server) && (args.server.indexOf('https:') && (is_secure || args.url.indexOf('https:')))) {
+        server_url = args.server;
+      } else server_url = "//instatimeline.appspot.com/";
+    
+      server_url += "?callback=?";
+    
+      // Using jquery-jsonp (http://code.google.com/p/jquery-jsonp/) for the request
+      // so that errors can be handled
+      $.jsonp({	
+        url: server_url,
+        data: { url: escape(args.url) },
+        dataType: 'jsonp',
+        timeout: 10000,
+        // It worked!
+        success: function(data, status) {
+      
+          // Create new, empty image
+          var return_image = new Image();
+        
+          // When the image has loaded
+          $(return_image).load(function(){
+          
+            // Set image dimensions
+            this.width = data.width;
+            this.height = data.height;
+          
+            // Return the image
+            if(typeof(args.success) == typeof(Function)) {
+              args.success(this);
+            }
+          
+          // Put the base64 encoded image into the src to start the load
+          }).attr('src', data.data);
+        
+        },
+        // Something went wrong.. 
+        error: function(xhr, text_status){
+          // Return the error(s)
+          if(typeof(args.error) == typeof(Function)) {
+            args.error(xhr, text_status);
+          }
+        }
+      });
+    
+    // No URL specified so error
+    } else {
+      if(typeof(args.error) == typeof(Function)) {
+        args.error(null, "no_url");
+      }
+    }
+  };
 
 })(jQuery);
