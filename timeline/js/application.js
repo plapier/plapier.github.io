23af9085b5278a49406e1bb60286d67364f64a04
Phil LaPier <phil@thoughtbot.com>
2012-10-14 01:08:33 -0400

    Changed to a function so it can be called elsewhere.

--- timeline/js/application.js
+++ timeline/js/application.js
@@ -1,19 +1,41 @@
-$(window).load(function () {
+getImageColor = function () {
   $("article.photo").each(function() {
-    var image_url = $(this).find("section.post-content img").attr("src");
-    var small_image_url= image_url.replace('500.jpg','100.jpg');
     var currentId = $(this).attr("id");
 
-    $.getImageData({
-      url: small_image_url,
-      success: function(image){
-        // Do something with new local version of the image
-        var dominantColor = getDominantColor(image);
-        $('#' + currentId + " section.post-meta h2").css('color', 'rgba('+ dominantColor +', 1)');
-      },
-      error: function(xhr, text_status){
-        console.log("Failed on " + small_image_url);
-      }
-    });
+    if ( $(this).find("h2").attr("style")) {
+    }
+    else {
+      var image_url = $(this).find("section.post-content img").attr("src");
+      var small_image_url = image_url.replace('500.jpg','100.jpg');
+
+      $.getImageData({
+        url: small_image_url,
+        success: function(image){
+          // Do something with new local version of the image
+          var dominantColor = getDominantColor(image);
+          $('#' + currentId + " section.post-meta h2").css('color', 'rgba('+ dominantColor +', 1)');
+        },
+        error: function(xhr, text_status){
+          console.log("Failed on " + small_image_url);
+        }
+      });
+    }
   });
+};
+
+$(window).load(function () {
+
+  // Run getImageColor function on load
+  getImageColor();
+
+  // Hijack infinite scroll & run getImageColor again
+  XMLHttpRequest.prototype.originalSend=XMLHttpRequest.prototype.send;
+  XMLHttpRequest.prototype.send=function(s){
+    this.addEventListener('load',function(){
+
+      getImageColor();
+
+    },false);
+    this.originalSend(s);
+  }
 });
